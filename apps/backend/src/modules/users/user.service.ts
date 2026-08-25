bbe36f6972be0fa7143c95cf4251cc058ac200ea
Arturo Blanco <100625719+arturobdev@users.noreply.github.com>
2026-08-25 20:40:36 -0300

    TSA-3: * replace password hash TODO with real encryption

--- apps/backend/src/modules/users/user.service.ts
+++ apps/backend/src/modules/users/user.service.ts
@@ -1,3 +1,4 @@
+import { type BaseEncryption } from "~/libs/modules/encryption/base-encryption.module.js";
 import { type Service } from "~/libs/types/types.js";
 import { UserEntity } from "~/modules/users/user.entity.js";
 import { type UserRepository } from "~/modules/users/user.repository.js";
@@ -9,20 +10,29 @@ import {
 } from "./libs/types/types.js";
 
 class UserService implements Service {
+	private encryption: BaseEncryption;
+
 	private userRepository: UserRepository;
 
-	public constructor(userRepository: UserRepository) {
+	public constructor(
+		userRepository: UserRepository,
+		encryption: BaseEncryption,
+	) {
 		this.userRepository = userRepository;
+		this.encryption = encryption;
 	}
 
 	public async create(
 		payload: UserSignUpRequestDto,
 	): Promise<UserSignUpResponseDto> {
+		const salt = this.encryption.generateSalt();
+		const hash = await this.encryption.hash(payload.password, salt);
+
 		const item = await this.userRepository.create(
 			UserEntity.initializeNew({
 				email: payload.email,
-				passwordHash: "HASH", // TODO
-				passwordSalt: "SALT", // TODO
+				passwordHash: hash,
+				passwordSalt: salt,
 			}),
 		);
 
