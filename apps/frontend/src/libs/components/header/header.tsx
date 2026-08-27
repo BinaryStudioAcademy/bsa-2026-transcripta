24227f7b96e9bb7ee25d169e73a6fbdd994f0b89
IlliaMatushchak <coldnight100@gmail.com>
2026-08-27 17:53:50 +0300

    TSA-12: + add Header component markup

--- apps/frontend/src/libs/components/header/header.tsx
+++ apps/frontend/src/libs/components/header/header.tsx
@@ -1,7 +1,36 @@
+import { Link } from "~/libs/components/components.js";
+import { AppRoute } from "~/libs/enums/enums.js";
+import headerLogo from "~/assets/img/logo.svg";
+
 const Header: React.FC = () => {
 	return (
-		<header>
-			<span>Transcripta</span>
+		<header className="header">
+			<span className="header-brand">
+				<img alt="Transcripta logo" className="header-logo" src={headerLogo} />
+				<span className="header-title">Transcripta</span>
+			</span>
+
+			<nav aria-label="Main navigation" className="header-nav">
+				<ul className="header-nav__list">
+					<li>
+						<a className="header-link" href="#how-it-works">
+							How it works
+						</a>
+					</li>
+
+					<li>
+						<a className="header-link" href="#document-types">
+							Document types
+						</a>
+					</li>
+
+					<li>
+						<Link className="header-link__btn" to={AppRoute.SIGN_UP}>
+							Sign up
+						</Link>
+					</li>
+				</ul>
+			</nav>
 		</header>
 	);
 };
