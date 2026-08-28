8154bc7ea8e19e27328add99a70a811b8f291261
Matistb <stebematias@gmail.com>
2026-08-28 16:00:38 -0300

    TSA-13: * use LogoIcon in sidebar brand

--- apps/frontend/src/libs/components/sidebar/sidebar.tsx
+++ apps/frontend/src/libs/components/sidebar/sidebar.tsx
@@ -1,5 +1,6 @@
 import { NavLink, useNavigate } from "react-router-dom";
 
+import { LogoIcon } from "~/libs/components/components.js";
 import { AppRoute } from "~/libs/enums/enums.js";
 import { useAppDispatch, useCallback } from "~/libs/hooks/hooks.js";
 import { storage, StorageKey } from "~/libs/modules/storage/storage.js";
@@ -23,18 +24,7 @@ const Sidebar: React.FC = () => {
 	return (
 		<aside className="sidebar">
 			<div className="sidebar__brand">
-				<span className="sidebar__brand-tile">
-					<svg
-						height="18"
-						viewBox="0 0 48 48"
-						width="18"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path d="M7 12 L41 8.5 L39.5 17 L8.5 19.5 Z" fill="#FFFDF8" />
-						<path d="M19.5 15.5 L29 14.5 L26 42 L22.5 42 Z" fill="#FFFDF8" />
-						<circle cx="33.5" cy="38.5" fill="#FFFDF8" r="3.4" />
-					</svg>
-				</span>
+				<LogoIcon size="medium" />
 				<span className="sidebar__brand-wordmark">Transcripta</span>
 			</div>
 
