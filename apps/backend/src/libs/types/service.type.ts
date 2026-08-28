041c71a566b04923536f54257bf013502ab2533d
Yehor Makarenko <mak.yegor@gmail.com>
2026-08-28 15:48:40 +0300

    TSA-10: - unnecessary type union

--- apps/backend/src/libs/types/service.type.ts
+++ apps/backend/src/libs/types/service.type.ts
@@ -1,7 +1,7 @@
 type Service<T = unknown> = {
 	create(payload: unknown): Promise<T>;
 	delete(): Promise<boolean>;
-	find(id: null | number): Promise<T>;
+	find(id: number): Promise<T>;
 	findAll(): Promise<{
 		items: T[];
 	}>;
