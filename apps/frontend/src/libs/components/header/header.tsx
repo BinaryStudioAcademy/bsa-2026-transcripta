28ca805277795bbc17a47b53345b174af50d51b9
IlliaMatushchak <coldnight100@gmail.com>
2026-08-29 21:43:45 +0300

    TSA-156: + make the header logo link to the root route

--- apps/frontend/src/libs/components/header/header.tsx
+++ apps/frontend/src/libs/components/header/header.tsx
@@ -5,10 +5,10 @@ import { AppRoute } from "~/libs/enums/enums.js";
 const Header: React.FC = () => {
 	return (
 		<header className="header">
-			<span className="header-brand">
+			<Link className="header-brand" to={AppRoute.ROOT}>
 				<img alt="Transcripta logo" className="header-logo" src={headerLogo} />
 				<span className="header-title">Transcripta</span>
-			</span>
+			</Link>
 
 			<nav aria-label="Main navigation" className="header-nav">
 				<ul className="header-nav__list">
