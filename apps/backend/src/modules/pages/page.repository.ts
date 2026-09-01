f29dbec9c2d660d241ab1cb244a40508ce656d12
Arturo Blanco <100625719+arturobdev@users.noreply.github.com>
2026-08-31 23:08:42 -0300

    TSA-113: + page model and repository

---
+++ apps/backend/src/modules/pages/page.repository.ts
@@ -0,0 +1,10 @@
+import { PageModel } from "./page.model.js";
+
+class PageRepository {
+	private pageModel: typeof PageModel;
+
+	public constructor(pageModel: typeof PageModel) {
+		this.pageModel = pageModel;
+	}
+}
+export { PageRepository };
