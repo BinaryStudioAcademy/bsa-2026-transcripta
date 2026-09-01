d2285b87043a088d9880306997ef839bf29f451c
IlliaMatushchak <coldnight100@gmail.com>
2026-09-01 18:04:23 +0300

    TSA-168: + update app routing logic & connect empty landing page

--- apps/frontend/src/libs/components/app/app.tsx
+++ apps/frontend/src/libs/components/app/app.tsx
@@ -1,23 +1,39 @@
 import { AUTH_ROUTES } from "~/libs/components/app/libs/constants/auth-routes.constant.js";
-import { Header, RouterOutlet, Sidebar } from "~/libs/components/components.js";
-import { AppRoute } from "~/libs/enums/enums.js";
-import { useLocation } from "~/libs/hooks/hooks.js";
+import {
+	LoaderOverlay,
+	RouterOutlet,
+	Sidebar,
+} from "~/libs/components/components.js";
+import { AppRoute, DataStatus } from "~/libs/enums/enums.js";
+import { useAppSelector, useLocation } from "~/libs/hooks/hooks.js";
 import { type ValueOf } from "~/libs/types/types.js";
+import { Landing } from "~/pages/landing/landing.js";
 
 const App: React.FC = () => {
 	const { pathname } = useLocation();
 
+	const user = useAppSelector((state) => state.auth.user);
+	const authStatus = useAppSelector((state) => state.auth.dataStatus);
+	const isAuthenticated = Boolean(user);
+
 	const isAuthPage = AUTH_ROUTES.has(pathname as ValueOf<typeof AppRoute>);
+
+	if (authStatus === DataStatus.PENDING) {
+		return <LoaderOverlay />;
+	}
 
 	if (isAuthPage) {
 		return <RouterOutlet />;
+	}
+
+	if (!isAuthenticated) {
+		return <Landing />;
 	}
 
 	return (
 		<div className="app-layout">
 			<Sidebar />
 			<main className="app-main">
-				{pathname === AppRoute.ROOT && <Header />}
 				<RouterOutlet />
 			</main>
 		</div>
---
+++ apps/frontend/src/pages/landing/landing.tsx
@@ -0,0 +1,9 @@
+const Landing: React.FC = () => {
+	return (
+		<>
+			<div>Lending</div>
+		</>
+	);
+};
+
+export { Landing };
