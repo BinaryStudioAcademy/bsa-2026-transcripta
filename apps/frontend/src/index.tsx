ff1ecc6b9452f5d7674bc6d956bd28e4eefaa477
IlliaMatushchak <coldnight100@gmail.com>
2026-09-02 15:35:04 +0300

    TSA-9: * move restoreDession dispatch to App to fix loader show

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -11,13 +11,10 @@ import {
 } from "~/libs/components/components.js";
 import { AppRoute } from "~/libs/enums/enums.js";
 import { store } from "~/libs/modules/store/store.js";
-import { restoreSession } from "./modules/auth/slices/actions.js";
 import { Auth } from "~/pages/auth/auth.jsx";
 import { Documents } from "~/pages/documents/documents.jsx";
 import { NotFound } from "~/pages/not-found/not-found.jsx";
 import { Test } from "~/pages/test/test.jsx";
-
-store.instance.dispatch(restoreSession());
 
 createRoot(document.querySelector("#root") as HTMLElement).render(
 	<StrictMode>
--- apps/frontend/src/libs/components/app/app.tsx
+++ apps/frontend/src/libs/components/app/app.tsx
@@ -1,13 +1,34 @@
 import { AUTH_ROUTES } from "~/libs/components/app/libs/constants/auth-routes.constant.js";
-import { Header, RouterOutlet, Sidebar } from "~/libs/components/components.js";
+import {
+	Header,
+	LoaderOverlay,
+	RouterOutlet,
+	Sidebar,
+} from "~/libs/components/components.js";
 import { AppRoute } from "~/libs/enums/enums.js";
-import { useLocation } from "~/libs/hooks/hooks.js";
+import {
+	useAppDispatch,
+	useAppSelector,
+	useEffect,
+	useLocation,
+} from "~/libs/hooks/hooks.js";
 import { type ValueOf } from "~/libs/types/types.js";
+import { selectIsInitialized } from "~/modules/auth/auth.js";
+import { restoreSession } from "~/modules/auth/slices/actions.js";
 
 const App: React.FC = () => {
 	const { pathname } = useLocation();
-
+	const dispatch = useAppDispatch();
+	const isInitialized = useAppSelector(selectIsInitialized);
 	const isAuthPage = AUTH_ROUTES.has(pathname as ValueOf<typeof AppRoute>);
+
+	useEffect(() => {
+		void dispatch(restoreSession());
+	}, [dispatch]);
+
+	if (!isInitialized) {
+		return <LoaderOverlay />;
+	}
 
 	if (isAuthPage) {
 		return <RouterOutlet />;
