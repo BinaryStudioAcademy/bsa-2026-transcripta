9aaf80bb21a30919001f83efeb352f5265b05d9f
IlliaMatushchak <coldnight100@gmail.com>
2026-09-02 13:56:11 +0300

    TSA-9: + enhance error handling middleware to manage unauthorized access

--- apps/frontend/src/libs/modules/middlewares/error-handling.middleware.ts
+++ apps/frontend/src/libs/modules/middlewares/error-handling.middleware.ts
@@ -1,20 +1,29 @@
 import { createListenerMiddleware, isRejected } from "@reduxjs/toolkit";
-import { ServerErrorType } from "@transcripta/shared";
+import { HTTPCode, ServerErrorType } from "@transcripta/shared";
 
 import { notification } from "~/libs/modules/notification/notification.js";
 import { SerializedAppError } from "~/libs/types/serialized-app-error.type.js";
 
 import { DEFAULT_ERROR_MESSAGE } from "./libs/constants/constants.js";
+import { storage, StorageKey } from "../storage/storage.js";
+import { actions } from "~/modules/auth/auth.js";
 
 const errorHandlingMiddleware = createListenerMiddleware();
 
 errorHandlingMiddleware.startListening({
-	effect: (action) => {
+	effect: async (action, listenerApi) => {
 		if (action.meta.aborted || action.meta.condition) {
 			return;
 		}
 
 		const error = action.error as SerializedAppError;
+
+		if ("status" in error && error.status === HTTPCode.UNAUTHORIZED) {
+			await storage.drop(StorageKey.TOKEN);
+			listenerApi.dispatch(actions.logout());
+
+			return;
+		}
 
 		if (!("errorType" in error)) {
 			notification.error(DEFAULT_ERROR_MESSAGE);
