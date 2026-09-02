472d42e18d4ba8e4b2308af06920d6ed6272685e
IlliaMatushchak <coldnight100@gmail.com>
2026-09-02 16:35:25 +0300

    TSA-9: - remove unnecessary check from ProtectedRoute

--- apps/frontend/src/libs/components/protected-route/protected-route.tsx
+++ apps/frontend/src/libs/components/protected-route/protected-route.tsx
@@ -1,20 +1,11 @@
 import { Navigate, Outlet } from "react-router-dom";
 
-import { LoaderOverlay } from "~/libs/components/components.js";
 import { AppRoute } from "~/libs/enums/enums.js";
 import { useAppSelector } from "~/libs/hooks/hooks.js";
-import {
-	selectIsAuthenticated,
-	selectIsInitialized,
-} from "~/modules/auth/auth.js";
+import { selectIsAuthenticated } from "~/modules/auth/auth.js";
 
 const ProtectedRoute: React.FC = () => {
-	const isInitialized = useAppSelector(selectIsInitialized);
 	const isAuthenticated = useAppSelector(selectIsAuthenticated);
-
-	if (!isInitialized) {
-		return <LoaderOverlay />;
-	}
 
 	if (!isAuthenticated) {
 		return <Navigate replace to={AppRoute.SIGN_IN} />;
