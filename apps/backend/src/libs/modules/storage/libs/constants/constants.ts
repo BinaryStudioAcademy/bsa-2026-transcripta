ace1cfd2b2be97ba5e93a2f49546cc2eb711cda5
Yehor Makarenko <mak.yegor@gmail.com>
2026-09-03 10:55:41 +0300

    TSA-74: * update MinIO storage with document download & send page methods

--- apps/backend/src/libs/modules/storage/libs/constants/constants.ts
+++ apps/backend/src/libs/modules/storage/libs/constants/constants.ts
@@ -1,1 +1,4 @@
+export { PAGE_FILE_NAME_LENGTH } from "./page-file-name-length.constant.js";
 export { SignedUrlConfig } from "./signed-url-config.constant.js";
+export { TMPDIR_PREFIX } from "./tmpdir-prefix.constant.js";
+export { TMPFILE_NAME } from "./tmpfile-name.constant.js";
--- apps/backend/src/libs/modules/storage/libs/types/storage.type.ts
+++ apps/backend/src/libs/modules/storage/libs/types/storage.type.ts
@@ -4,9 +4,27 @@ import {
 } from "./types.js";
 
 type Storage = {
+	downloadToTempFolder(sourceKey: string): Promise<{
+		clear: () => Promise<void>;
+		filePath: string;
+	}>;
 	getUploadSignedUrl(
 		options: UploadSignedUrlRequest,
 	): Promise<UploadSignedUrlResponse>;
+	sendPage({
+		documentId,
+		page,
+		pageImage,
+		pageThumbnail,
+	}: {
+		documentId: number;
+		page: number;
+		pageImage: Buffer;
+		pageThumbnail: Buffer;
+	}): Promise<{
+		imageKey: string;
+		thumbnailKey: string;
+	}>;
 };
 
 export { type Storage };
