a3e48c9b5c802c0f1cfebdd6edc34353833587e1
Arturo Blanco <100625719+arturobdev@users.noreply.github.com>
2026-09-05 10:35:46 -0300

    TSA-127: + page verification endpoint

--- apps/backend/src/modules/pages/pages.ts
+++ apps/backend/src/modules/pages/pages.ts
@@ -1,8 +1,29 @@
+import { logger } from "~/libs/modules/logger/logger.js";
+
+import { DocumentModel } from "../documents/document.model.js";
+import { DocumentRepository } from "../documents/document.repository.js";
+import { TranscriptionModel } from "./../transcription/transcription.model.js";
+import { TranscriptionRepository } from "./../transcription/transcription.repository.js";
+import { PageEventModel } from "./page-event/page-event.model.js";
+import { PageEventRepository } from "./page-event/page-event.repository.js";
+import { PageController } from "./page.controller.js";
 import { PageModel } from "./page.model.js";
 import { PageRepository } from "./page.repository.js";
+import { PageService } from "./page.service.js";
+
+const documentRepository = new DocumentRepository(DocumentModel);
+const transcriptionRepository = new TranscriptionRepository(TranscriptionModel);
+const pageEventRepository = new PageEventRepository(PageEventModel);
 
 const pageRepository = new PageRepository(PageModel);
 
-export { PageEntity } from "./page.entity.js";
-export { PageRepository } from "./page.repository.js";
-export { pageRepository };
+const pageService = new PageService({
+	documentRepository,
+	pageEventRepository,
+	pageRepository,
+	transcriptionRepository,
+});
+
+const pageController = new PageController(logger, pageService);
+
+export { pageController, pageRepository };
