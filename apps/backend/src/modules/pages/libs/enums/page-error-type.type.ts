d690d177ec44755f00b50678b8f2433aa6d22bf8
Arturo Blanco <100625719+arturobdev@users.noreply.github.com>
2026-09-05 11:24:23 -0300

    TSA-127: * adress minor review feedback

--- apps/backend/src/modules/pages/libs/enums/page-error-type.type.ts
+++
@@ -1,5 +0,0 @@
-const PageErrorType = {
-	PAGE_EVENT_ONCE: "page_event_once",
-} as const;
-
-export { PageErrorType };
--- apps/backend/src/modules/pages/libs/types/page-verify-payload.ts
+++
@@ -1,8 +0,0 @@
-import { VerifyPageRequestDto } from "@transcripta/shared";
-
-type VerifyPagePayload = VerifyPageRequestDto & {
-	pageId: number;
-	userId: number;
-};
-
-export { VerifyPagePayload };
