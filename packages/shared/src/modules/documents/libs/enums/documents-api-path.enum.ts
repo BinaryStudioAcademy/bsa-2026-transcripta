67284f3b00ff08908289f27b29063b5295169f34
Yehor Makarenko <mak.yegor@gmail.com>
2026-09-09 15:32:04 +0300

    TSA-119: * fix DocumentApiPath

--- packages/shared/src/modules/documents/libs/enums/documents-api-path.enum.ts
+++ packages/shared/src/modules/documents/libs/enums/documents-api-path.enum.ts
@@ -1,11 +1,10 @@
-/* eslint-disable perfectionist/sort-objects */
 const DocumentsApiPath = {
 	BY_ID: "/:id",
 	BY_ID_PAGES: "/:id/pages",
 	INGEST: "/:id/ingest",
-	ROOT: "/",
 	PAUSE: "/:id/pause",
 	RESUME: "/:id/resume",
+	ROOT: "/",
 } as const;
 
 export { DocumentsApiPath };
