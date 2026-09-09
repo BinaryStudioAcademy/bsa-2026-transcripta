755ca0a2f8e75e0a43fec937e3a0ec94c4f77f05
Yehor Makarenko <mak.yegor@gmail.com>
2026-09-09 22:37:44 +0300

    TSA-235: + new pdf error messages

--- apps/backend/src/libs/modules/pdf-page-processor/libs/enums/error-message.enum.ts
+++ apps/backend/src/libs/modules/pdf-page-processor/libs/enums/error-message.enum.ts
@@ -1,5 +1,8 @@
 const ErrorMessage = {
-	FAILED_TO_GET_PAGE_COUNT: "Failed to get page count",
+	FAILED_TO_CONVERT_PAGE:
+		"Failed to convert PDF page to image. File may be corrupted",
+	FAILED_TO_GET_PAGE_COUNT: "Failed to get page count. File may be corrupted",
+	FAILED_TO_READ_PDF: "Failed to read PDF. It may be corrupted",
 } as const;
 
 export { ErrorMessage };
