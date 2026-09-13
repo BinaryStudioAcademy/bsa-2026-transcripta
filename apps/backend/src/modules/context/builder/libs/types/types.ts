d307b2a474bf0defcfe4197ea893fd1fd2ce5a16
Yehor Makarenko <mak.yegor@gmail.com>
2026-09-13 21:02:55 +0300

    TSA-114: + context builder types

---
+++ apps/backend/src/modules/context/builder/libs/types/types.ts
@@ -0,0 +1,5 @@
+export { type BuiltContext } from "./built-context.type.js";
+export { type ContextBuilder } from "./context-builder.type.js";
+export { type LexiconEntry } from "./lexicon-entry.type.js";
+export { type SeedGlossaryEntry } from "./seed-glossary-entry.type.js";
+export { type PageWithText } from "~/modules/pages/pages.js";
