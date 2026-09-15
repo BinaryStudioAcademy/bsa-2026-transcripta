61f6f1fd7f68ec542e2ffa265bbaad196c16f61f
Arturo Blanco <100625719+arturobdev@users.noreply.github.com>
2026-09-15 06:59:34 -0300

    TSA-142: + reprocessable page status support

--- apps/backend/src/modules/pages/libs/constants/constants.ts
+++ apps/backend/src/modules/pages/libs/constants/constants.ts
@@ -1,2 +1,3 @@
 export { CLOSED_PAGE_STATUSES } from "./closed-page-statuses.constant.js";
 export { NUMBER_OF_PAGES_TO_INCREMENT } from "./page-number-increment.constant.js";
+export { REPROCESSABLE_PAGE_STATUSES } from "./reprocessable-page-statuses.constant.js";
