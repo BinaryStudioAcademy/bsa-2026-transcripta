3afd0149363bab54258e4ceb0a2d64fc55b8723b
Yehor Makarenko <mak.yegor@gmail.com>
2026-09-22 14:40:39 +0300

    TSA-140: + rederiveStructuredQueue

--- apps/backend/src/libs/modules/queue/libs/enums/queue-name.enum.ts
+++ apps/backend/src/libs/modules/queue/libs/enums/queue-name.enum.ts
@@ -1,6 +1,7 @@
 const QueueName = {
 	DOCUMENT_CLEANUP: "document.cleanup",
 	PAGE_TRANSCRIBE: "page.transcribe",
+	REDERIVE_STRUCTURED: "page.rederive-structured",
 } as const;
 
 export { QueueName };
