a7abd3df09f1ecdd7275f820fa081ab4d91b738e
IgnacioRamírez <ignaciobart2@gmail.com>
2026-09-22 17:04:36 -0600

    TSA-281: * prevent marking document as done for failed first page while still ingesting

--- apps/backend/src/modules/documents/document.repository.ts
+++ apps/backend/src/modules/documents/document.repository.ts
@@ -248,6 +248,7 @@ class DocumentRepository {
 			.query(trx)
 			.patch({ status: DocumentStatus.DONE })
 			.where({ id })
+			.whereNot("status", DocumentStatus.INGESTING)
 			.whereNotExists(openPages)
 			.execute();
 	}
