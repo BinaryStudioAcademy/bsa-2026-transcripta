588b4b457b07512077810deafb7c984396648c30
IlliaMatushchak <coldnight100@gmail.com>
2026-09-22 13:49:50 +0300

    TSA-337: + show preceding pages in verification strip

--- apps/frontend/src/pages/verification/verification.tsx
+++ apps/frontend/src/pages/verification/verification.tsx
@@ -37,8 +37,9 @@ import {
 	MIN_NUMBER_OF_PAGES,
 	PAGE_STEP,
 } from "./libs/constants/verification.constants.js";
-import { useVerificationKeyboard } from "./libs/hooks/use-verification-keyboard.hook.js";
+import { getPagesFrom } from "./libs/helpers/get-pages-from.helper.js";
 import "./verification.css";
+import { useVerificationKeyboard } from "./libs/hooks/use-verification-keyboard.hook.js";
 import {
 	type EditConflictDraft,
 	type PageVerificationActionValue,
@@ -122,7 +123,7 @@ const Verification: React.FC = () => {
 			pageActions.loadPages({
 				documentId: document.id,
 				query: {
-					from: cursorPageNo,
+					from: getPagesFrom(cursorPageNo),
 					limit: MAX_LOADED_PAGES,
 				},
 			}),
