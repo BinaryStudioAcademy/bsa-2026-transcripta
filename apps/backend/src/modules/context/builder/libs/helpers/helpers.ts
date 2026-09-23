10b147fd0ebe991955490a951a5058563e69f04f
IgnacioRamírez <ignaciobart2@gmail.com>
2026-09-23 00:07:11 -0600

    TSA-369: * changing more variables with container type apart from those indicated

--- apps/backend/src/modules/context/builder/libs/helpers/helpers.ts
+++ apps/backend/src/modules/context/builder/libs/helpers/helpers.ts
@@ -1,5 +1,5 @@
 export { createContextHash } from "./create-context-hash.helper.js";
-export { isStringArray } from "./is-string-array.helper.js";
+export { isStringCollection } from "./is-string-collection.helper.js";
 export { renderLexiconEntry } from "./render-lexicon-entry.helper.js";
 export { renderLexicon } from "./render-lexicon.helper.js";
 export { renderNeighbouringPagesEntry } from "./render-neighbouring-pages-entry.helper.js";
--- apps/backend/src/modules/context/builder/libs/helpers/is-string-collection.helper.ts
+++ apps/backend/src/modules/context/builder/libs/helpers/is-string-collection.helper.ts
@@ -1,10 +1,10 @@
 import { STRING_TYPE_NAME } from "../constants/constants.js";
 
-const isStringArray = (value: unknown): value is string[] => {
+const isStringCollection = (value: unknown): value is string[] => {
 	return (
 		Array.isArray(value) &&
 		value.every((item) => typeof item === STRING_TYPE_NAME)
 	);
 };
 
-export { isStringArray };
+export { isStringCollection };
