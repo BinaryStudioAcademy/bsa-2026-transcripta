fca370ed7fc3940d17987a7a243181a0dd9debb9
Arturo Blanco <100625719+arturobdev@users.noreply.github.com>
2026-09-23 22:33:45 -0300

    TSA-269: * load presets from API in upload form

--- apps/frontend/src/libs/hooks/hooks.ts
+++ apps/frontend/src/libs/hooks/hooks.ts
@@ -11,7 +11,7 @@ export {
 	useRef,
 	useState,
 } from "react";
-export { useController as useFormController } from "react-hook-form";
+export { useController as useFormController, useWatch } from "react-hook-form";
 export {
 	matchPath,
 	useLocation,
