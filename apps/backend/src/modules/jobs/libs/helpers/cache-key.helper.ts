ff19be6f619ff29a04fa8cfb25dbedb297274d17
illiaboiko <illiaboiko.095@gmail.com>
2026-09-25 21:06:41 +0300

    TSA-475: * include system prompt hash in transcription cache key

--- apps/backend/src/modules/jobs/libs/helpers/cache-key.helper.ts
+++ apps/backend/src/modules/jobs/libs/helpers/cache-key.helper.ts
@@ -1,7 +1,10 @@
 import { sha256 } from "~/modules/context/libs/helpers/hash.helper.js";
+import { SYSTEM_PROMPT } from "~/modules/transcription/libs/constants/constants.js";
 
 import { CACHE_KEY_SEPARATOR } from "../constants/constants.js";
 import { type CacheKeyParts } from "../types/types.js";
+
+const SYSTEM_PROMPT_HASH = sha256(SYSTEM_PROMPT);
 
 const buildCacheKey = ({
 	contextHash,
@@ -10,7 +13,9 @@ const buildCacheKey = ({
 	presetHash,
 }: CacheKeyParts): string =>
 	sha256(
-		[imageSha256, presetHash, modelId, contextHash].join(CACHE_KEY_SEPARATOR),
+		[imageSha256, presetHash, modelId, contextHash, SYSTEM_PROMPT_HASH].join(
+			CACHE_KEY_SEPARATOR,
+		),
 	);
 
 export { buildCacheKey };
